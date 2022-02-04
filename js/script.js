07cfa49e6c72889a31d3861ea713c5d8ebb5e844
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-02-04 16:05:27 +0100

    created getRandomEmail

--- js/script.js
+++ js/script.js
@@ -3,6 +3,18 @@ Vue.config.devtools = true;
 const root = new Vue({
     el: '#root',
     data: {
+        emails: [],
+        totalEmails: 10,
 
-    }
+    },
+    methods: {
+        getRandomEmail() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(res => {
+                this.emails.push(res.data.response);
+                console.log(res.data.response);
+            });
+        },
+
+    },
+
 })
